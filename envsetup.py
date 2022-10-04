38fac7f098f775f7544a30f2061524624892d528
lorossi <mail@lorenzoros.si>
2022-10-04 16:38:12 +0200

    cleaned code

--- envsetup.py
+++ envsetup.py
@@ -1,7 +1,16 @@
-from pathlib import Path
-from re import M, sub
-from sys import argv
+"""This module is associated to the latex notes template.
 
+It is needed to:
+    - clean the tikzit styles file
+    - create the temporary folder for the tikz images
+    - delete the temporary folder
+    - delete the temporary files associated with the tex document
+"""
+from sys import argv
+from re import M, sub
+from pathlib import Path
+
+# constants
 TEMP_FOLDERS = ["tikztemp"]
 TEMP_EXTENSIONS = ["aux", "auxlock", "log", "pdf", "out", "toc"]
 PLACEHOLDER_EXT = ".placeholder"
@@ -9,7 +18,7 @@ ARROW_STYLE = "{Triangle[scale=0.8]}"
 
 
 def clean_styles() -> None:
-    """cleans the tikzstyles file as it was created by tikzit"""
+    """Clean the tikzstyles file as it was created by tikzit."""
     # load styles files
     with open("tikzit.tikzstyles", "r") as f:
         data = f.read()
@@ -31,12 +40,14 @@ def clean_styles() -> None:
 
 
 def make_folder() -> None:
+    """Create the temporary folder for the tikz images."""
     for f in TEMP_FOLDERS:
         Path(f).mkdir(parents=True, exist_ok=True)
         Path(f"{f}/{PLACEHOLDER_EXT}").touch()
 
 
 def delete_folder() -> None:
+    """Delete the temporary folder, thus deleting all the rendered tikz images."""
     for folder in TEMP_FOLDERS:
         for file in Path(folder).iterdir():
             file.unlink()
@@ -44,12 +55,14 @@ def delete_folder() -> None:
 
 
 def delete_temp() -> None:
+    """Delete all the temporary files associated with the tex document images."""
     for e in TEMP_EXTENSIONS:
         for file in Path(".").glob(f"*.{e}"):
             file.unlink(missing_ok=True)
 
 
 def main(argv: list[str]) -> None:
+    """Read the arguments and call the appropriate functions."""
     if "deletetemp" in argv:
         delete_temp()
         delete_folder()
